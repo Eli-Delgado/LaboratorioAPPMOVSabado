655c1df7e1745301a836d851f3f46e79b69ade1c
Eli-Delgado <eli.israel@outlook.com>
2019-09-28 08:58:26 -0500

    Practica Login

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; //Importado
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  registrarForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    //para nuestro validador vamos a crear un grupo, para validar cada campo
+    this.registrarForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmacionContrasena: ['', Validators.required]
 
-  ngOnInit() {
+    /*}, {
+      validator: this.MustMatch('password', 'confirm')*/
+    });
   }
 
+  ngOnInit() {//Funcion por defecto de angular, al momento que entras a la pagina se ejecuta. 
+  }
+
+  get f() { return this.registrarForm.controls; }
+
+  registrar() {
+    console.log(this.registrarForm.valid);
+  }
+  /*
+  registrar(form) {
+    let inputs = form.form.value;
+    if (inputs.password != inputs.confirm) {
+      console.log('La constraseña y confirmar contraseña no son iguales');
+    }
+  }
+  */
+  /*
+  MustMatch(controlName: string, matchingControlName: string) {
+    return true;
+  }
+  */
 }
